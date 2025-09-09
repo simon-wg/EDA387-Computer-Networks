e4caf021ef863d22413445b687afce63b6fe59ae
Simon Westlin Green <simon.green@live.se>
2025-09-09 08:14:05 +0200

    resolv

--- Lab1/Lab1.1/lab1-resolve-skel.cpp
+++ Lab1/Lab1.1/lab1-resolve-skel.cpp
@@ -2,7 +2,7 @@
 /** Translate host name into IPv4
  *
  * Resolve IPv4 address for a given host name. The host name is specified as
- * the first command line argument to the program. 
+ * the first command line argument to the program.
  *
  * Build program:
  *  $ g++ -Wall -g -o resolve <file>.cpp
@@ -10,91 +10,99 @@
 /******************************************************************* -}}}1- */
 
 #include <cstddef>
+#include <cstdlib>
+#include <err.h>
 #include <netinet/in.h>
-#include <stdio.h>
 #include <stddef.h>
+#include <stdio.h>
 
+#include <arpa/inet.h>
 #include <assert.h>
 #include <limits.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
-#include <arpa/inet.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 //--//////////////////////////////////////////////////////////////////////////
 //--    local declarations          ///{{{1///////////////////////////////////
 
-void print_usage( const char* aProgramName );
+void print_usage(const char *aProgramName);
 
 //--    local config                ///{{{1///////////////////////////////////
 
 /* HOST_NAME_MAX may be missing, e.g. if you're running this on an MacOS X
- * machine. In that case, use MAXHOSTNAMELEN from <sys/param.h>. Otherwise 
+ * machine. In that case, use MAXHOSTNAMELEN from <sys/param.h>. Otherwise
  * generate an compiler error.
  */
 #if !defined(HOST_NAME_MAX)
-#	if defined(__APPLE__)
-#		include <sys/param.h>
-#		define HOST_NAME_MAX MAXHOSTNAMELEN
-#	else  // !__APPLE__
-#		error "HOST_NAME_MAX undefined!"
-#	endif // ~ __APPLE__
+#if defined(__APPLE__)
+#include <sys/param.h>
+#define HOST_NAME_MAX MAXHOSTNAMELEN
+#else // !__APPLE__
+#error "HOST_NAME_MAX undefined!"
+#endif // ~ __APPLE__
 #endif // ~ HOST_NAME_MAX
 
 //--    main()                      ///{{{1///////////////////////////////////
-int main( int aArgc, char* aArgv[] )
-{
-	// Check$ if the user supplied a command line argument.
-	if( aArgc != 2 )
-	{
-		print_usage( aArgv[0] );
-		return 1;
-	}
+int main(int aArgc, char *aArgv[]) {
+    // Check$ if the user supplied a command line argument.
+    if (aArgc != 2) {
+        print_usage(aArgv[0]);
+        return 1;
+    }
 
-	// The (only) argument is the remote host that we should resolve.
-	const char* remoteHostName = aArgv[1];
+    // The (only) argument is the remote host that we should resolve.
+    const char *remoteHostName = aArgv[1];
 
-	// Get the local host's name (i.e. the machine that the program is 
-	// currently running on).
-	const size_t kHostNameMaxLength = HOST_NAME_MAX+1;
-	char localHostName[kHostNameMaxLength];
+    // Get the local host's name (i.e. the machine that the program is
+    // currently running on).
+    const size_t kHostNameMaxLength = HOST_NAME_MAX + 1;
+    char localHostName[kHostNameMaxLength];
 
-	if( -1 == gethostname( localHostName, kHostNameMaxLength ) )
-	{
-		perror( "gethostname(): " );
-		return 1;
-	}
+    if (-1 == gethostname(localHostName, kHostNameMaxLength))
+        err(EXIT_FAILURE, "gethostname");
 
-	// Print the initial message
-	printf( "Resolving `%s' from `%s':\n", remoteHostName, localHostName );
+    // Print the initial message
+    printf("Resolving `%s' from `%s':\n", remoteHostName, localHostName);
 
-	// TODO : add your code here
-	// Create a hints struct to pass to getaddrinfo()
-	struct addrinfo hints;
-	struct addrinfo *results;
-	hints.ai_family	= AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = 0;
-	hints.ai_addr = NULL;
-	hints.ai_addrlen = 0;
-	hints.ai_canonname = NULL;
-	hints.ai_next = NULL;
-	int addr_status = getaddrinfo(remoteHostName, NULL, hints, &results);
-	printf("Addrinfo: %d", hints.ai_family);
-	// getaddrinfo() allocates a addrinfo struct which needs to be freed	
-	freeaddrinfo();
-	// Ok, we're done. Return success.
-	return 0;
+    // TODO : add your code here
+    // Create a hints struct to pass to getaddrinfo()
+    struct addrinfo hints;
+    struct addrinfo *results;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = 0;
+    hints.ai_addr = NULL;
+    hints.ai_addrlen = 0;
+    hints.ai_canonname = NULL;
+    hints.ai_next = NULL;
+    int addr_status = getaddrinfo(remoteHostName, NULL, &hints, &results);
+    if (0 != addr_status) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(addr_status));
+        exit(EXIT_FAILURE);
+    };
+
+    sockaddr *sockAddr = results->ai_addr;
+    assert(sockAddr->sa_family == hints.ai_family);
+    sockaddr_in *inAddr = (sockaddr_in *)sockAddr;
+
+    uint32_t ipNumber = inAddr->sin_addr.s_addr;
+    char ipv4addr[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &ipNumber, ipv4addr, INET_ADDRSTRLEN);
+
+    printf("%s has address %s\n", remoteHostName, ipv4addr);
+    // getaddrinfo() allocates a addrinfo struct which needs to be freed
+    freeaddrinfo(results);
+    // Ok, we're done. Return success.
+    return 0;
 }
-
 
 //--    print_usage()               ///{{{1///////////////////////////////////
-void print_usage( const char* aProgramName )
-{
-	fprintf( stderr, "Usage: %s <hostname>\n", aProgramName );
+void print_usage(const char *aProgramName) {
+    fprintf(stderr, "Usage: %s <hostname>\n", aProgramName);
 }
 
-//--///}}}1/////////////// vim:syntax=cpp:foldmethod=marker:ts=4:noexpandtab: 
+//--///}}}1/////////////// vim:syntax=cpp:foldmethod=marker:ts=4:noexpandtab:
--- flake.nix
+++ flake.nix
@@ -1,54 +1,136 @@
 {
   description = "A Nix-flake-based C/C++ development environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs =
-    inputs:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-      forEachSupportedSystem =
-        f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          f {
-            pkgs = import inputs.nixpkgs { inherit system; };
-          }
-        );
-    in
-    {
-      devShells = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default =
-            pkgs.mkShell.override
-              {
-                # Override stdenv in order to change compiler:
-                # stdenv = pkgs.clangStdenv;
-              }
-              {
-                packages = with pkgs; [
-                  clang-tools
-                  cmake
-                  codespell
-                  conan
-                  cppcheck
-                  doxygen
-                  gdb
-                  gcc
-                  glibc
-                  gtest
-                  lcov
-                  man
-                  man-pages
-                  vcpkg
-                  vcpkg-tool
-                ];
-              };
-        }
-      );
-    };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {allowUnfree = true;};
+        };
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # Compilers and build tools
+              gcc
+              clang
+              cmake
+              ninja # Fast build system alternative to make
+              meson # Modern build system
+
+              # Development tools
+              clang-tools # clangd, clang-format, clang-tidy
+              gdb
+              lldb # LLVM debugger (alternative to gdb)
+              valgrind # Memory debugging
+              rr # Record and replay debugger
+
+              # Static analysis and linting
+              cppcheck
+              clang-analyzer
+
+              # Documentation
+              doxygen
+              graphviz # For doxygen graphs
+              man
+              man-pages
+              man-pages-posix
+
+              # Package managers and libraries
+              conan
+              vcpkg
+              vcpkg-tool
+              pkg-config # Essential for finding libraries
+
+              # Testing frameworks
+              gtest
+              catch2
+              criterion # C testing framework
+
+              # Code coverage and profiling
+              lcov
+              gcovr # Alternative coverage tool
+              perf-tools # Performance profiling
+
+              # Utilities
+              codespell
+              bear # Generate compile_commands.json for clangd
+              ccls # Alternative language server
+
+              # Libraries your project uses
+              readline
+              termcap
+            ];
+
+            # Set up environment variables
+            shellHook = ''
+              echo "🚀 C/C++ Development Environment"
+              echo "Available compilers: gcc $(gcc --version | head -1 | cut -d' ' -f4), clang $(clang --version | head -1 | cut -d' ' -f4)"
+              echo "Build systems: cmake, ninja, meson"
+              echo "Debuggers: gdb, lldb"
+              echo "Static analysis: cppcheck, clang-tidy"
+              echo ""
+
+              # Set up compile_commands.json for better IDE support
+              export CMAKE_EXPORT_COMPILE_COMMANDS=1
+
+              # Useful aliases
+              alias cmake-debug='cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1'
+              alias cmake-release='cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1'
+              alias ninja-debug='cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1'
+            '';
+          };
+
+          # Minimal shell for CI/automated builds
+          ci = pkgs.mkShell {
+            packages = with pkgs; [
+              gcc
+              cmake
+              ninja
+              gtest
+            ];
+          };
+
+          # Clang-focused development shell
+          clang = pkgs.mkShell.override {stdenv = pkgs.clangStdenv;} {
+            packages = with pkgs; [
+              clang
+              clang-tools
+              lldb
+              cmake
+              ninja
+              libcxx
+            ];
+
+            shellHook = ''
+              echo "🦙 Clang-based C/C++ Environment"
+              export CC=clang
+              export CXX=clang++
+            '';
+          };
+        };
+
+        # Optional: Define packages for your actual projects
+        packages = {
+          # Example of how you might build your lsh project
+          # lsh = pkgs.stdenv.mkDerivation {
+          #   pname = "lsh";
+          #   version = "0.1.0";
+          #   src = ./.;
+          #   nativeBuildInputs = [ pkgs.cmake ];
+          #   buildInputs = with pkgs; [ readline termcap ];
+          # };
+        };
+      }
+    );
 }
